622a92f2cd3925c88f526588ab7dfcaf51cd921f
santhosh-2504 <santhoshanantha58@gmail.com>
2025-06-12 17:33:34 +0530

    App updated

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import entryRoutes from './routes/entryRoutes.js';
 import notificationRoutes from './routes/notificationRoutes.js';
+import analyticsRoutes from './routes/analyticsRoutes.js';
 
 connectDB();
 
@@ -25,5 +26,6 @@ app.use(express.static('public'));
 app.use('/api/users', userRoutes);
 app.use('/api/entries', entryRoutes);
 app.use('/api/notifications', notificationRoutes);
+app.use('/api/analytics', analyticsRoutes);
 
 export default app;
