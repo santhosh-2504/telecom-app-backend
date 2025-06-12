3505ec2a6c5db68bbe466aefda1bcdee3bc0350f
santhosh-2504 <santhoshanantha58@gmail.com>
2025-06-12 15:54:58 +0530

    Vercel specific changes

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+// import app from './app.js';
+
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+// server.js
 import app from './app.js';
 
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default function handler(req, res) {
+  return app(req, res); // Important for Vercel's serverless model
+}
